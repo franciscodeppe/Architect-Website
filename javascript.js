2dd5b405bb1b363d50917d9238d3e14b04cfc58b
francisco <francisco.deppe@gmail.com>
2017-06-20 16:35:42 -0700

    more updates

--- javascript.js
+++ javascript.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
                 width: "33%"
             });
         };
-		
+
     });
 })
 
@@ -58,66 +58,67 @@ var dark = ['#872729', '#d45f33', '#b1200f']
 
 var light = ['dacd43', '5b9f90', 'd45f33', 'white']
 
-var intervalId1;
-var intervalId2;
-var intervalId3;
-
-var color1;
-var color2;
-var color3;
-
-
-$.cssHooks.backgroundColor = {
-    get: function(elem) {
-        if (elem.currentStyle)
-            var bg = elem.currentStyle["backgroundColor"];
-        else if (window.getComputedStyle)
-            var bg = document.defaultView.getComputedStyle(elem,
-                null).getPropertyValue("background-color");
-        if (bg.search("rgb") == -1)
-            return bg;
-        else {
-            bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-
-            function hex(x) {
-                return ("0" + parseInt(x).toString(16)).slice(-2);
-            }
-            return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
-        }
-    }
-}
+// $.cssHooks.backgroundColor = {
+//     get: function(elem) {
+//         if (elem.currentStyle)
+//             var bg = elem.currentStyle["backgroundColor"];
+//         else if (window.getComputedStyle)
+//             var bg = document.defaultView.getComputedStyle(elem,
+//                 null).getPropertyValue("background-color");
+//         if (bg.search("rgb") == -1)
+//             return bg;
+//         else {
+//             bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+//
+//             function hex(x) {
+//                 return ("0" + parseInt(x).toString(16)).slice(-2);
+//             }
+//             return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+//         }
+//     }
+// }
 
 function changeColorArmet() {
-    color1 = colorPallete[1]
-    console.log(1, color1)
-    $("#armet").css({
-        "background-color": colorPallete[color1]
-    })
-    intervalId2 = setInterval(changeColorDavis, 500)
-    clearInterval(intervalId1)
+    if (colorPallete.length === 0) {
+        colorPallete = ['#f28423', '#685b4b', '#487271', '#e3d982', "#ac573a", "#80a57c", "#907558", "#9ea874"]
+    }
+    var n = Math.floor(Math.random() * colorPallete.length)
+	$("#armet").animate({
+    backgroundColor: colorPallete[n],
+}, 1000);
+
+    colorPallete.splice(n, 1)
+	console.log(colorPallete[n])
+	setTimeout(changeColorDavis, 5000)
 }
 
 function changeColorDavis() {
-    color2 =
-        console.log(2, color2)
-        $("#davis").css({
-            "background-color": colorPallete[color2]
-        })
-    clearInterval(intervalId2)
-    intervalId3 = setInterval(changeColorNewlove, 500)
+    if (colorPallete.length === 0) {
+        colorPallete = ['#f28423', '#685b4b', '#487271', '#e3d982', "#ac573a", "#80a57c", "#907558", "#9ea874"]
+    }
+    var n = Math.floor(Math.random() * colorPallete.length)
+	$("#davis").animate({
+    backgroundColor: colorPallete[n],
+}, 1000);
+    colorPallete.splice(n, 1)
+	console.log(colorPallete[n])
+	setTimeout(changeColorNewlove, 5000)
 }
 
 function changeColorNewlove() {
-    color3 =
-        console.log(3, color3)
-        $("#newlove").css({
-            "background-color": colorPallete[color3]
-        })
-    clearInterval(intervalId3)
-    run()
-}
+    if (colorPallete.length === 0) {
+        colorPallete = ['#f28423', '#685b4b', '#487271', '#e3d982', "#ac573a", "#80a57c", "#907558", "#9ea874"]
+    }
+    var n = Math.floor(Math.random() * colorPallete.length)
+	$("#newlove").animate({
+    backgroundColor: colorPallete[n],
+}, 1000);
+    colorPallete.splice(n, 1)
+	console.log(colorPallete[n])
+	setTimeout(changeColorArmet, 5000)
 
+}
 function run() {
-    intervalId1 = setInterval(changeColorArmet, 500)
+	setTimeout(changeColorArmet, 5000)
 }
 run()
